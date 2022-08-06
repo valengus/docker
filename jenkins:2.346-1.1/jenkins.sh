4fc240cb784d33e38b71923ca781e45c9aab4475
Valentin Gusachenko <vgusachenko@gmail.com>
2022-08-06 09:58:22 +0300

    commit

--- jenkins:2.346-1.1/jenkins.sh
+++ jenkins:2.346-1.1/jenkins.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -e
 
-echo Done
+exec echo Done "$@"
 
 # exec /usr/bin/java -Dcasc.jenkins.config=${JENKINS_HOME}/casc_configs -Djenkins.install.runSetupWizard=false -jar /usr/share/java/jenkins.war --webroot=/var/cache/jenkins/war --httpPort=${http_port} "$@"
