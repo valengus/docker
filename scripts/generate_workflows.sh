2f2d8c307fec7c605c8d98c834636a3ddc3e262e
Valentin Gusachenko <vgusachenko@gmail.com>
2022-08-07 16:41:47 +0300

    commit

--- scripts/generate_workflows.sh
+++ scripts/generate_workflows.sh
@@ -1,17 +1,19 @@
 #!/bin/bash 
 
 echo "Cleaning up previous workflows"
-rm -f .github/workflows/*
+# rm -f .github/workflows/*
 
 containers=( $(ls | grep :) )
 
 for container in "${containers[@]}"
 do
-  echo "${container}"
-  if [ -d "${container}" ] 
+
+  if [ -f "${container}/Dockerfile" ] 
     then
-      echo "Directory ${container} exists."
-      echo "Creating workflow yml filefor ${container}"
-      touch .github/workflows/${container}
+      echo "Dockerfile in ${container} exists."
+      echo "Creating workflow yml file for ${container}"
+      touch .github/workflows/${container}.yml
+    else
+      echo "Dockerfile in ${container} doesnt exists."
   fi
 done
