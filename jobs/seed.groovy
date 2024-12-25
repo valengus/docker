dc5a72bce14a8eae59f5c4a7ef35d767f17c3b91
Gusachenko Valentin <vgusachenko@gmail.com>
2024-12-25 20:12:55 +0200

    commit

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -1,7 +1,12 @@
-import jenkins.model.Jenkins
+import jenkins.model.*
+import hudson.model.*
+import hudson.triggers.*
 import com.cloudbees.hudson.plugins.folder.*
-import org.jenkinsci.plugins.workflow.job.WorkflowJob
+import org.jenkinsci.plugins.workflow.job.*
+import org.jenkinsci.plugins.workflow.cps.*
 
+  
+  
 def gitUrl = "https://github.com/valengus/docker.git"
 
 Jenkins jenkins = Jenkins.instance 
@@ -47,6 +52,73 @@ pkrvars.each { it ->
 
 println(imageList)
 
+
 imageList.each { it ->
-  job = folder.createProject(WorkflowJob, it.image)
+
+  // Jenkinsfile
+  def jenkinsfileContent = """
+  pipeline {
+    agent any
+    environment {
+      PKR_VAR_docker_registry = 'docker-registry:5000'
+    }
+    stages {
+      stage('Checkout') {
+        steps {
+          checkout([
+            \$class: 'GitSCM',
+            doGenerateSubmoduleConfigurations: false,
+            extensions: [[\$class: 'CloneOption', noTags: true, shallow: true, depth: 1, timeout: 30]],
+            userRemoteConfigs: [[ url: 'https://github.com/valengus/docker.git' ]],
+            branches: [ [name: 'main'] ]
+          ])
+        }
+      }
+      stage('Prepare') {
+        steps {
+          sh '/usr/bin/packer init docker.pkr.hcl'
+        }
+      }
+      // stage('Build') {
+      //   steps {
+      //     sh "/usr/bin/packer build -var-file=vars/${it.image}.pkrvars.hcl -only=\'*.docker.build\' docker.pkr.hcl"
+      //   }
+      // }
+      // stage('Test') {
+      //   steps {
+      //     echo 'Test'
+      //   }
+      // }
+      // stage('Push') {
+      //   steps {
+      //     sh "/usr/bin/packer build -var-file=vars/${it.image}.pkrvars.hcl -only=\'docker.push\' docker.pkr.hcl"
+      //   }
+      // }
+    }
+  }
+  """
+
+  // Создаем новый job типа Pipeline
+  def job = folder.createProject(WorkflowJob, it.image)
+  // Устанавливаем содержание Jenkinsfile
+  def flowDefinition = new CpsFlowDefinition(jenkinsfileContent, true)
+  job.definition = flowDefinition
+
+  
+  // Сохраняем изменения
+  job.save()
 }
+
+// // Trigers
+// imageList.each { it ->
+
+//   if (it.dependsOn != null) {
+//     // println("Переменная не равна null")
+//     def triggeringJobName = it.dependsOn
+//     def triggeredJobName = it.image
+
+//   } else {
+//     // println("Переменная равна null")
+//   }
+
+// }
